16f5af5965fadc992b674ceaabe5c043509102c5
DJ-Bless <BDdorian@gmail.com>
2015-05-08 16:44:04 +0200

    test

---
+++ tp01_exo1.php
@@ -0,0 +1,25 @@
+<?php $dir = 'TP01'; include('../header.php'); ?>
+
+<h1>Etat civil</h1>
+<p>
+    <strong>Sexe</strong> : Homme<br />
+    <strong>Age</strong> : 20<br />
+    <strong>Nom</strong> : Marié<br />
+    <strong>Prenom</strong> : Dorian
+</p>
+
+<h1>Formations</h1>
+<h2>Lycée</h2>
+<p>
+    Lycée Louis Rascol : <i>1998-2005</i><br />
+    <font style="font-size: 18px">1<sup>er</sup></font> lycée de France
+</p>
+
+<h2>Université</h2>
+<p>
+    Nom : <i>Université de Technologie de Troyes</i><br/>
+    Spécialisation : <font color="red">ISI</font>
+</p>
+<h1>Expériences professionnelles</h1>
+
+<?php include('../footer.php'); ?>
